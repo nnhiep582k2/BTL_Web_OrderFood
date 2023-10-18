5d3cbbe97fe0e1dccc8774d8c4e00cc72733a900
Nguyen Ngoc Hiep <nnhiep582k2@gmail.com>
2023-10-18 08:48:05 +0700

    feat: complete base button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
-<template>Home View</template>
+<template>
+    <h2>DEMO CONTROL - nnhiep</h2>
+    <base-button :type="ButtonType.warning" text="Order Now" />
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue';
+import { ButtonType } from '@/enums/ButtonType';
+</script>
 
 <style lang="scss" scoped></style>
