c65ba6ee7fa4742f6b23d8ad8731b5296c4332c7
KienDev <ngotrungkien2k2@gmail.com>
2023-10-18 23:27:32 +0700

    build base control and add icon

---
+++ src/components/BaseSelectBox.vue
@@ -0,0 +1,78 @@
+<!-- Author: NTKIEN -->
+<template>
+    <select
+        :id="id"
+        :name="name"
+        class="select"
+        :class="classes"
+        :value="modelValue"
+        @input="
+            $emit(
+                'update:modelValue',
+                ($event?.target as HTMLInputElement).value
+            )
+        "
+    >
+        <option
+            :value="option.value"
+            class="option"
+            v-for="option in options"
+            :key="JSON.stringify(option)"
+        >
+            {{ option.label }}
+        </option>
+    </select>
+</template>
+
+<script setup lang="ts">
+import { PropType } from "vue";
+
+type OptionTypes = {
+    label: String;
+    value: String;
+};
+defineProps({
+    id: {
+        type: String,
+        required: false,
+    },
+    name: {
+        type: String,
+        required: false,
+    },
+    classes: {
+        type: String,
+        required: false,
+    },
+    modelValue: {
+        type: String,
+        required: false,
+    },
+    required: {
+        type: String,
+        required: false,
+    },
+    options: {
+        type: Array as PropType<OptionTypes[]>,
+        default: true,
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.select {
+    width: 150px;
+    height: var(--height-control);
+    user-select: none;
+    outline: none;
+    border-radius: 6px;
+    font-size: var(--fontsize-control);
+    border: 1px solid #ccc;
+    padding: 10px 12px 9px;
+
+    .option {
+        padding: 10px 12px 9px;
+        height: 100%;
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,49 @@
 <template>
     <h2>DEMO CONTROL - nnhiep</h2>
     <base-button :type="ButtonType.warning" text="Order Now" />
+    <BaseSelectBox :options="options" v-model="testing.value" />
+    <BaseCheckbox id="checkedTask" label="checked" v-model="testing.selected" />
+    <BaseRadioGroup
+        id="radioCheck"
+        label="radioCheck"
+        v-model="testing.radioCheck"
+        value="First"
+    />
+    <BaseRadioGroup
+        id="radioCheck2"
+        label="radioCheck2"
+        v-model="testing.radioCheck"
+        value="second"
+    />
 </template>
 
 <script setup lang="ts">
-import BaseButton from '@/components/BaseButton.vue';
-import { ButtonType } from '@/enums/ButtonType';
+import BaseButton from "@/components/BaseButton.vue";
+import { ButtonType } from "@/enums/ButtonType";
+import { reactive, watch } from "vue";
+import BaseSelectBox from "@/components/BaseSelectBox.vue";
+import BaseCheckbox from "@/components/BaseCheckbox.vue";
+import BaseRadioGroup from "@/components/BaseRadioGroup.vue";
+const testing = reactive({
+    value: "",
+    selected: "",
+    radioCheck: false,
+});
+
+const options = [
+    {
+        label: "Normal",
+        value: "Normal",
+    },
+    {
+        label: "high",
+        value: "high",
+    },
+];
+
+watch(testing, (_, __) => {
+    console.log(testing);
+});
 </script>
 
 <style lang="scss" scoped></style>
