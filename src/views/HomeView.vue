d14e12a4619d440250d49e47d96986714b169cdf
KienDev <ngotrungkien2k2@gmail.com>
2023-10-19 15:04:46 +0700

    update code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <h2>DEMO CONTROL - nnhiep</h2>
     <base-button :type="ButtonType.warning" text="Order Now" />
     <BaseSelectBox :options="options" v-model="testing.value" />
@@ -49,49 +48,6 @@ const options = [
 watch(testing, (_, __) => {
     console.log(testing);
 });
-=======
-    <div class="home-view">
-        <h2 class="m-b-10">DEMO CONTROL - nnhiep</h2>
-        <base-button
-            class="m-b-10"
-            :type="ButtonType.warning"
-            text="Order Now"
-        />
-        <div class="d-flex m-b-10">
-            <base-ads
-                v-for="(item, index) in DataBaseAds"
-                :src="item.src"
-                :key="index"
-            >
-                <template #content>
-                    <div class="title">{{ item.title }}</div>
-                    <div class="discount">{{ item.discount }}</div>
-                    <base-button
-                        :type="item.button.type"
-                        :text="item.button.text"
-                    />
-                </template>
-            </base-ads>
-        </div>
-        <base-promotion
-            class="m-b-10"
-            v-for="(item, index) in DataBasePromotions"
-            :src="item.src"
-            :key="index"
-            :title="item.title"
-            :datas="item.datas"
-        />
-    </div>
-</template>
-
-<script setup lang="ts">
-import BaseButton from '@/components/BaseButton.vue';
-import BaseAds from '@/components/BaseAds.vue';
-import BasePromotion from '@/components/BasePromotion.vue';
-import { ButtonType } from '@/enums/ButtonType';
-import { DataBaseAds } from '@/mocks/BaseAds';
-import { DataBasePromotions } from '@/mocks/BasePromotions';
->>>>>>> 028f3ade4e66880350aa989356a1eb1fd8b2c139
 </script>
 
 <style lang="scss" scoped>
