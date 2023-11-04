f7f65232fcb19fe63ab0ec10d430e51218a6bf5b
KienDev <ngotrungkien2k2@gmail.com>
2023-11-04 11:48:48 +0700

    delete action in admin

--- src/components/BaseTable.vue
+++ src/components/BaseTable.vue
@@ -22,7 +22,11 @@
                             :type="btn.type"
                             :text="btn.text"
                             @click="
-                                handleActionClick(btn.text, item[headers[0]])
+                                handleActionClick(
+                                    btn.text,
+                                    item[headers[0]],
+                                    item[headers[1]]
+                                )
                             "
                         />
                     </td>
@@ -37,8 +41,14 @@ import { ref, type PropType } from "vue";
 import BaseButton from "./BaseButton.vue";
 import { ButtonType } from "@/enums/ButtonType";
 import Swal from "sweetalert2";
+import http from "@/services/http/http";
+import { useStore } from "vuex";
+import { SET_LOADING } from "@/stores/storeConstants";
+import { router } from '@/router';
+import { useRoute } from 'vue-router';
 
-
+const store = useStore();
+const route = useRoute();
 const props = defineProps({
     headers: {
         type: Array as PropType<string[]>,
@@ -52,36 +62,49 @@ const props = defineProps({
 const alert = ref();
 const buttonAction = [
     {
-        text: 'Add',
+        text: "Add",
         type: ButtonType.success,
     },
     {
-        text: 'Edit',
+        text: "Edit",
         type: ButtonType.information,
     },
     {
-        text: 'Delete',
+        text: "Delete",
         type: ButtonType.warning,
     },
 ];
 
-const handleActionClick = (action: string, item: any) => {
+const handleActionClick = (action: string, item: any, data: string) => {
     if (action === "Add") {
     } else if (action === "Edit") {
     } else if (action === "Delete") {
         Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
+            title: `Are you sure you want to delete this ${data}?`,
             icon: "warning",
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
             confirmButtonText: "Yes, delete it!",
-        }).then((result) => {
+        }).then(async (result) => {
             if (result.isConfirmed) {
-                Swal.fire("Deleted!", "Your file has been deleted.", "success");
+                if (await handleDelete(item)) {
+                    Swal.fire("Deleted!", "Delete successfully.", "success");
+                    window.location.reload()
+                }
             }
         });
+    }
+};
+
+const handleDelete = async (id: string) => {
+    try {
+        store.dispatch(SET_LOADING, true);
+        const { data } = (await http.delete("/Auth/deleteById/" + id)).data;
+        store.dispatch(SET_LOADING, false);
+        return data;
+    } catch (error) {
+        store.dispatch(SET_LOADING, false);
     }
 };
 </script>
